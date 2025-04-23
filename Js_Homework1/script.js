e34b53973d8e9651e9a6dccacdf7574d37404976
Timofey Loskutov <kretosyt00@gmail.com>
2025-04-23 10:08:07 +0300

    fist homework

--- Js_Homework1/script.js
+++ Js_Homework1/script.js
@@ -1,3 +1,26 @@
 //19 номер, работа с переменными и объектами
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+// const a = prompt('Один из последних просмотренных фильмов?')
+// const b = prompt('Один из последних просмотренных фильмов?')
+// const c = prompt('На сколько оцените его?')
+// const d = prompt('На сколько оцените его?')
+
+
+for (let i = 0; i < 2; i++){
+    answerOfFilm = prompt('Один из последних просмотренных фильмов?')
+}
+for (let i = 0; i < 2; i++){
+    answerOfRate = prompt('На сколько оцените его?')
+}
+personalMovieDB.movies[answerOfFilm] = answerOfRate
+
+console.log(personalMovieDB)
